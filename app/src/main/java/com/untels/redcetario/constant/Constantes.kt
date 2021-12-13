5c79d3982229b0b598b9b4d31daf6c14f483ab87
Jibaru <ignacioruedaboada@hotmail.com>
2021-12-12 23:35:32 -0500

    Creadas constantes

--- app/src/main/java/com/untels/redcetario/constant/Constantes.kt
+++ app/src/main/java/com/untels/redcetario/constant/Constantes.kt
@@ -1,2 +1,11 @@
 package com.untels.redcetario.constant
 
+const val API_BASE = "https://redcetario.herokuapp.com"
+const val OBTENER_RECETAS = "/api/recetas"
+const val OBTENER_RECETA = "/api/receta/{id}"
+const val LOGIN = "/api/login"
+const val REGISTRO = "/api/registro"
+const val PUBLICAR_COMENTARIO = "/api/recetas/{id}/comentario"
+const val OBTENER_NOTIFICACIONES = "/api/notificaciones"
+const val ELIMINAR_NOTIFICACION = "/api/notificacion/{id}"
+const val ACTUALIZAR_PERFIL = "/api/cliente/{id}"
