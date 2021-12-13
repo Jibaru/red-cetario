12734f7f4e596ff970cfd761435645cb47719f12
Jibaru <ignacioruedaboada@hotmail.com>
2021-12-12 23:36:12 -0500

    Creado servicio

--- app/src/main/java/com/untels/redcetario/service/RecetaService.kt
+++ app/src/main/java/com/untels/redcetario/service/RecetaService.kt
@@ -1,4 +1,27 @@
 package com.untels.redcetario.service
 
+import com.untels.redcetario.constant.API_BASE
+import com.untels.redcetario.constant.OBTENER_RECETA
+import com.untels.redcetario.constant.OBTENER_RECETAS
+import okhttp3.*
+
 class RecetaService {
+    private val client = OkHttpClient()
+
+    fun obtenerTodos(callback: Callback) {
+        val request = Request.Builder()
+            .url(API_BASE + OBTENER_RECETAS)
+            .build()
+
+        client.newCall(request).enqueue(callback)
+    }
+
+    fun obtener(idReceta: Int, callback: Callback) {
+        val url = API_BASE + OBTENER_RECETA.replace("{id}", idReceta.toString())
+        val request = Request.Builder()
+            .url(url)
+            .build()
+
+        client.newCall(request).enqueue(callback)
+    }
 }
