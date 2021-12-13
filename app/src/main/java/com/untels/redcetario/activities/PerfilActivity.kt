d1a439c72c73da2eb13732f0fcbf8fb965c50781
Jibaru <ignacioruedaboada@hotmail.com>
2021-12-13 16:56:18 -0500

    Configurada actividad perfil parcialmente

--- app/src/main/java/com/untels/redcetario/activities/PerfilActivity.kt
+++ app/src/main/java/com/untels/redcetario/activities/PerfilActivity.kt
@@ -2,11 +2,31 @@ package com.untels.redcetario.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.untels.redcetario.R
+import com.untels.redcetario.databinding.ActivityPerfilBinding
+import com.untels.redcetario.service.ServiceManager
 
 class PerfilActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPerfilBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_perfil)
+        binding = ActivityPerfilBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnGuardarPerfil.setOnClickListener {
+            // TODO: Guardar perfil y actualizar activity
+        }
+
+        setupCliente()
+    }
+
+    private fun setupCliente() {
+        ServiceManager.getAutenticacionService().getCliente()?.let {
+            binding.tvNombreCompleto.text = (it.nombre + " " + it.apePaterno + " " + it.apeMaterno)
+            binding.txtNombrePrefil.setText(it.nombre)
+            binding.txtApePaternoPerfil.setText(it.apePaterno)
+            binding.txtApeMaternoPerfil.setText(it.apeMaterno)
+            binding.txtCorreoElectronicoPerfil.setText(it.correoElectronico)
+        }
     }
 }
