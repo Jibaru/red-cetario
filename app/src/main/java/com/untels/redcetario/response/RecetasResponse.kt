c2056d3a599c5b7fd787fcecdcc2f1dec3013770
Jibaru <ignacioruedaboada@hotmail.com>
2021-12-12 23:36:26 -0500

    Creado respuesta

--- app/src/main/java/com/untels/redcetario/response/RecetasResponse.kt
+++ app/src/main/java/com/untels/redcetario/response/RecetasResponse.kt
@@ -1,4 +1,9 @@
 package com.untels.redcetario.response
 
-class RecetasResponse {
-}
+import com.untels.redcetario.model.RecetaCabecera
+import java.io.Serializable
+
+data class RecetasResponse(
+    val ok: Boolean,
+    val recetas: List<RecetaCabecera>
+) : Serializable
