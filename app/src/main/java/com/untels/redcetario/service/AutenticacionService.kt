4caaeb447c64bcfbd50aae1dbd60d254d15b200f
Jibaru <ignacioruedaboada@hotmail.com>
2021-12-13 16:54:06 -0500

    Creado AutenticacionService

--- app/src/main/java/com/untels/redcetario/service/AutenticacionService.kt
+++ app/src/main/java/com/untels/redcetario/service/AutenticacionService.kt
@@ -1,4 +1,25 @@
 package com.untels.redcetario.service
 
-class AutenticacionService {
+import com.google.gson.Gson
+import com.untels.redcetario.model.Cliente
+
+class AutenticacionService(private val gson: Gson) {
+    private var cliente: Cliente? = null
+
+    fun setCliente(cliente: Cliente) {
+        this.cliente = cliente
+    }
+
+    fun isAutenticado(): Boolean {
+        return cliente != null
+    }
+
+    fun getCliente(): Cliente? {
+        return cliente
+    }
+
+    fun limpiarCliente() {
+        cliente = null
+    }
+
 }
