024d9c849d89ae5a40b6febd9defc06217b984fe
Jibaru <ignacioruedaboada@hotmail.com>
2021-12-13 22:09:56 -0500

    Creados responses

--- app/src/main/java/com/untels/redcetario/response/ActualizarClienteResponse.kt
+++ app/src/main/java/com/untels/redcetario/response/ActualizarClienteResponse.kt
@@ -1,4 +1,9 @@
 package com.untels.redcetario.response
 
-class ActualizarClienteResponse {
-}
+import com.untels.redcetario.model.Cliente
+import java.io.Serializable
+
+data class ActualizarClienteResponse(
+    val ok: Boolean,
+    val cliente: Cliente
+) : Serializable
--- app/src/main/java/com/untels/redcetario/response/NotificacionesResponse.kt
+++ app/src/main/java/com/untels/redcetario/response/NotificacionesResponse.kt
@@ -1,4 +1,9 @@
 package com.untels.redcetario.response
 
-class NotificacionesResponse {
-}
+import com.untels.redcetario.model.Notificacion
+import java.io.Serializable
+
+data class NotificacionesResponse(
+    val ok: Boolean,
+    val notificaciones: List<Notificacion>
+) : Serializable
